cc91dba6e854e52301eccee7183f862afbeb63af
salantorn <salantornlol@gmail.com>
2025-04-18 17:41:24 +0700

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3384290229"
+    serviceWorkerVersion: "446513660"
   }
 });
