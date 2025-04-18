99d874b35b3ebe15b7707f9fa677a5bd781c8bcf
salantorn <salantornlol@gmail.com>
2025-04-18 17:31:14 +0700

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "21ab8a448f66e382257b048f27fc8c28
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "6466b290953604520c4d07d5b70f8c2f",
+"flutter_bootstrap.js": "7e8d1e6ba9e0f431d9bb9bf723a3b94b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
